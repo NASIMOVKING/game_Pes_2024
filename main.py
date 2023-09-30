59476d248e576bef0d2d159e8d9bbd64e25f69d9
NASIMOVKING <146510782+NASIMOVKING@users.noreply.github.com>
2023-09-30 10:58:34 +0500

    Python

---
+++ main.py
@@ -0,0 +1,793 @@
+# a = 215
+# b = 2
+# print("hello samarkand")
+# print("hi")
+
+# hafta = int(input("hafta kunini kirit:"))
+# if hafta == 1:
+#     print("dushanba")
+# elif hafta == 2:
+#     print("seshanba")
+# elif hafta == 3:
+#     print("chorshanba")
+# elif hafta == 4:            
+#     print("payshanba")
+# elif hafta == 5:    
+#     print("juma")
+# elif hafta == 6:
+#     print("shanba")
+# elif hafta == 7:
+#     print("yakshanba")
+# else:
+#     print("bunaqa hafta kuni yuq")
+
+# tuple 
+# a = 35
+# tp =(1,2,3,4,5,6,6)
+# tp1 = ("shaxzoda","malika")
+# print(tp,tp1)
+# tp =("farangiz","farida","jasur","hilola","kamila")
+# print(tp[0])
+# print(tp[1])
+# print(tp[2])
+# print(tp[3])
+# print(tp[4])
+
+# tp = (1,2,3,4,5,)
+# tp1 = (6,7,8,9,10,)
+# print(tp[0]*tp1[0])
+# print(tp[1]*tp1[1])
+# print(tp[2]*tp1[2])
+# print(tp[3]*tp1[3])
+# print(tp[4]*tp1[4])
+
+
+# tp1 = (1,2,3,4,5,6,7,8,9,10,)
+# print(tp1[0:2])
+# print(tp1[2:4])
+# print(tp1[4:6])
+# print(tp1[6:8])
+# print(tp1[8:10])
+
+# tpl =("ali","vali","solid","ahmad","zoire","himmat")
+# for i in tpl:
+#     print(i)
+
+# tp = (4,4,5,5,5,3,3,3,8,8,8,8,9,9,9,10,10,1,0,10,)
+# print(tp.count(5))
+
+# tp = (1,2,3,4,5,5,)
+# num = int(input("son kirit"))
+# s = 0
+# for i in tp:
+#     if num == i:
+#         s+=1
+# print(s)
+
+
+# tp = (4,4,4,3,3,2,4,5,6)
+# num = int(input("son kirit:"))
+# for i in range(len(tp)):
+#     if num == tp[i]:
+#        print(i)
+#        break
+
+
+
+# parol ="2345"
+# pr = input("parol kirit")
+# if pr==parol:
+#     print("xush kelibsan ")
+
+
+# z = 1000
+# if z == 100:
+#     print("z is 100")
+# elif z == 200:
+#      print("z is 200")
+# elif z == 300:
+#      print("z is 300")
+# elif z == 1000:     
+#     print("z is 1000")
+# else:
+#     print("z is unknown")
+
+# m = 26
+# n = 27
+# if m > n:
+#     print('m is greater than n')
+# elif m == n:
+#     print('m is equal to n')
+# else:
+#     print('m is less than n')
+
+# from pyclbr import Function
+# from this import d
+
+
+# p = 30
+# q = 20
+
+# if ((p<q) | (p==q)):
+#     print("Either p is less than q or p is equal to q")
+# elif ((p==30) & (q==30)):
+#     print("p and q are equal")
+# elif (p!=q):
+#     print("p is not equal to q")
+# else:
+#     print("p is greater than q")
+
+
+#     if True:
+#     print("Statement is True")
+# else:
+#     print("Statement is False")
+
+# z = 10
+
+# if z<0:
+#     print("z is less than 0")
+# else:
+#     if z < 5:
+#         print("z is less than 5")
+#     else:
+#         print("z is greater than 5")
+
+# count = 200
+
+# if count < 400:
+#     print('The count is below 400')
+#     if count < 300:
+#         print('The count is below 300')
+#     else:
+#         print('The count is less than 400 and greater than or equal to 300')
+
+# print("Enter a number: ")
+# number = int(input())
+
+# if number < 20:
+#     print("The number is less than 20")
+# elif number == 20:
+#     print("The number is equal to 20")
+# else:
+#     print("The number is greater than 20")
+
+# for i in "Shaxob":
+#     print(i)
+
+# for j in range(5):
+#     print(j)
+
+
+# Animallist = ["cat","dog","tiger","cow"]
+# for x in Animallist:
+#       print(x)
+
+# numbers = [13,25,30,60,80,89,90,78]
+# sum = 0
+# for n in numbers:
+#     sum +=n
+# print(sum)
+
+
+# for i in range(1,5):
+#     for j in range(i):
+#         print("+",end="")
+#     print() 
+
+# 1 mavzu: Function
+
+# def hi():
+#     print("hello")
+#     print("hi")
+#     for i in range(10):
+#           print(i)
+
+# hi()
+
+# b = 5
+# c = 2
+# def hi():
+#     print(b+c)
+# hi()
+
+# d = 1
+# f = 9
+# def a():
+#     print(d*f)
+# a()    
+
+# g = 100
+# t = 8
+# def b(): 
+#      print(g/t)
+# b()  
+
+# h = 7
+# p = 9
+# def r():
+#     print(h-p)
+# r()
+
+# def qushish(a,b):
+#     print(a+b)
+# qushish(2,3)
+# qushish(4,5)
+# qushish(1,2)
+
+# a = 16
+# b = 20
+# def ayirish(a,b,c,d):
+#     print(a-b-c-d)
+# ayirish(1,2,3,4)    
+# # ayirish()
+# ayirish()
+
+# g = 100
+# f = 9
+# def kopaytirish(g,f):
+#     print(g*f)
+# kopaytirish()
+# kopaytirish()
+# kopaytirish()
+
+# d = 100
+# r = 15
+# def bolish(d,r):
+#     print(d/r)
+# bolish(100,2)
+# bolish(50,2)
+# bolish(20,2)
+
+#  g = 100
+# f = 9
+# def kopaytirish(g,f,d,r,s):
+#     print(g*f*d*r*s)
+# kopaytirish(1,2,3,4,5)
+
+# def fullname (name,family):
+#     print( f"Mening ismim {name} familya {family}")
+
+# fullname("Ronaldo","Cristiano")
+
+# def info (name,family,yosh,adress):
+#     print(f"Mening ismim: {name} familya: {family} yosh: {yosh} adress: {adress}")
+
+# info("shaxob","nasimov","23","urganji")
+
+# def info():
+#     print(2)
+#     return 2
+# print(info()+4)    
+
+# def son():
+#     b = int(input("sonni kirit"))
+#     print(b**2)
+#     print(b**3)
+# son()
+
+# def python():
+#     print("Welcome to the pythonista world")
+# python()
+
+# def addnumbers(x,y):
+#     sum = x + y
+#     return sum
+
+# output = addnumbers(12,9)
+# print(output)    
+
+
+# def myFruits(d1,d2,d3,d4,):
+#     Fruitslist = [d1,d2,d3,d4]
+#     return Fruitslist
+
+# output = myFruits("apple","banana","grapes","orange")
+# print(output)
+
+# def myveggies(v1,v2,v3):
+#     vegtuple = (v1,v2,v3)
+#     return  vegtuple
+# o = myveggies("carrot","potato","tomato")
+# print(o)
+
+# def car(name,model):
+#     print(name)
+#     print(model)
+# car(model="m5f90",name="bmw")
+
+
+# def car(name,model="m5f90"):
+#     print(name)
+#     print(model)
+# car("bmw")
+
+# def calendar (year,month,date=""):
+#     print(year,month,date)
+# calendar(2023,2)  
+
+
+
+# def qushish(a,b):
+#     print(a+b)
+# qushish(2,3)
+# qushish(4,5)
+# qushish(1,2)
+
+# def plus(*b):
+#     s = 1
+#     for i in b:
+#         s*=i
+#     print(s)
+# plus(1,2,3,4,5)
+    
+
+# def kank():
+#     a = int(input("birinchi sonni kirit:"))
+#     b = int(input("ikkinchi sonni kirit:"))
+#     amal = input('amalni yoz,(+,-,*,/):')
+#     if amal=="+":
+#         print(a+b)
+#     elif amal=="-":
+#         print(a-b)
+#     elif amal=="*":
+#         print(a*b)
+#     elif amal=="/":
+#         print(a/b)
+#     else:
+#         print("bunaqa amal yuq")
+
+# kank()
+
+
+# def plus(*b):
+#     for i in b:
+#         print(i)
+# plus(10,20,30)
+
+# def can (b,d):
+#     print(b+d)
+#     print(+-d)
+# can(56,15)
+
+# def max(a,b,c):
+
+# mavzu:dict   
+
+# car = "bmw"
+# studentka = {
+#     "name ": "dilshod",
+#     "family" : "Avezov",
+#     "age":23,
+#     "adress": "samarqand seliskiy",
+#     "kurs":4
+# }
+# print(studentka)
+
+# oquvchi = {
+#     "name":"Hilola",
+#     "family":"Esanova",
+#     "sinf":"10",
+#     "age":"22",
+#     "address":"samarqand tumani",
+
+# }
+# print(oquvchi["name"])
+# print(oquvchi["family"])
+# print(oquvchi["sinf"])
+# print(oquvchi["age"])
+# print(oquvchi["address"])
+
+# oquvchi = {
+#     "name":"Hilola",
+#     "family":"Esanova",
+#     "sinf":"10",
+#     "age":"22",
+#     "merage":True,
+#     "farzandlar":["Ali","Ahmad","Zafar","Azizaa"]
+# }
+# oquvchi["name"] = "Malika"
+# oquvchi["family"] = "Axmedova"
+# oquvchi["age"] = "33"
+# oquvchi["sinf"] = 9
+
+# print(oquvchi)
+
+# otam = {
+#     'ism':'Kamol',
+#     'family':'Kamolov',
+#     'yosh':39,
+#     'yili':1989
+# }
+# print('f'"Mening otam{otam['ism]} family{otam['family']} yosh{otam['39']) yili{otam['1989]}")
+
+# onam = {
+#     'ism':'Iroda',
+#     'family':'Kamolova',
+#     'yosh':37,
+#     'yili':1986
+# }
+# print('f'"Mening onam{onam['ism]} family{onam['family']} yosh{onam['37']) yili{onam['1986]}")
+
+# akam = {
+#     'ism':'shaxram',
+#     'family':'Nasimov',
+#     'yosh':24,
+#     'yili':1998
+# }
+# print('f'"Mening akam{akam['ism]} family{akam['family']} yosh{akam['24']) yili{akam['1998]}")
+
+# ukam = {
+#     'ism':'shaxzod',
+#     'family':'Nasimov',
+#     'yosh':17,
+#     'yili':2006
+# }
+# print('f'"Mening ukam{ukam['ism]} family{ukam['family']} yosh{ukam['17']) yili{ukam['2006]}")
+
+# taom = {
+#     'dadam':'palov',
+#     'oyim':'manti',
+#     'akam':'chuchvara',
+#     'ukam':'pitsa',
+#     'ozim':'farqi yuq'
+# }
+
+# print(taom)
+
+# slovar = {
+# 'hello': "bu salom berish uchun ishlatadigan soz",
+# 'phone': " bu odamlar bilan gaplashaidh uchun qurilma",
+# 'dog': "uyda boqiladigan hayvon",
+# 'cat': "uyda boqiladigan hayvon 1",
+# 'knife':'uyda kerak boladigan rozgor buyumi',
+# }
+# print(slovar['hello'])
+# print(slovar['phone'])
+# print(slovar['dog'])
+# print(slovar['cat'])
+# print(slovar['knife'])
+
+# car = {
+#     'name':"bmw",
+#     'year':2023,
+#     'speed':380,   
+# }
+# print(car.get('names'))
+# print(car.car['names'])
+
+# telefon ={
+#     "ism":"iphone",
+#     "model":"14 pro max",
+#     "rangi":"blue",
+#     "xotirasi":"512 gb",
+#     "yemrosti":"100",
+#     "sena":"1200$"
+# }
+# print(telefon)
+
+# num_dict = {
+#     1:11,2:22,3:22,4:33,5:33,6:33
+# }
+# print(num_dict)
+
+
+# cleardict = {1:"python",2:"csharp",3:"php"}
+# print("before clearing the dictionary:",cleardict)
+# cleardict.clear()
+# print("after clearing the dictionary:",cleardict)
+
+# alang ={1:"python",2:"csharp",3:"php"}
+# # if value exists,it returns the value 
+# print(alang.get(1))
+# # if value exists,it returns none
+# print(alang.get(4))
+
+# lang ={1:"python",2:"csharp",3:"php"}
+# print(lang.pop(3))
+# print(lang.pop(2))
+# # print(lang.values())
+# print(lang)
+
+# lang ={1:"python",2:"csharp",3:"php"}
+# del lang[1]
+# print(lang)
+
+# mydict = {"n1":100,"n2":200,"n3":300}
+# print(max(mydict.values()))
+# print(min(mydict.values()))
+
+
+
+# p = ("p1", "p2", "p3")
+# q = 5
+# mydict = dict.fromkeys(p, q,)
+# print(mydict)
+
+# squareddict ={}
+# for i in range (1,6):
+#     squareddict[i] =i**2
+# print(squareddict) 
+
+# dict_a = {1:"india",2:"usa",3:"uk",4:"canada"}
+# print(dictionary \"dict_a\'is...")
+# print(dict_a)
+
+
+# # print(dictionary \"dict_a\'keys...")
+# # for x in dict_a:
+# #     print(x)
+
+
+# # print(dictionary \"dict_a\'values...")
+# # for x in dict_a.values():
+# #     print(x)
+
+# # print(dictionary \"dict_a\'keys & values...")
+# # for x,y in dict_a.items():
+# #     print(x,':',y)
+
+# # a = 5
+# # print(5**4)
+
+
+# # a = 22
+# # b = 4
+# # print(a/b)
+
+# # matn ="hello world"
+# # print(matn)
+
+# # xabar ="Assalom Aleykum"
+# # print(xabar)
+
+# # radius = 5
+# # pi = 2.13159
+# # aylanma_yuzi = pi * radius**2
+# # print("radius", radius,  "ga teng aylananing yuzi=",aylanma_yuzi)
+
+
+# # a = int(input("istalgan son kiriting:"))
+# # print(a," ning kvadrati ","a**3""ga teng")
+# # print(a," ning kubi ","a**5""ga teng")
+
+
+# # yosh = int(input("Yoshingiz nechida:"))
+# # t_yil = 2023-yosh
+# # print("Siz ", t_yil, " da tug'ilgansiz")
+
+
+# # z = float(input("Birinchi sonni kiriting: "))
+# # f = float(input("Ikkinchi sonni kiriting: "))
+# # print(f"{z}+{f}=", z+f)
+# # print(f"{z}-{f}=", z-f)
+# # print(f"{z}x{f}=", z*f)
+# # print(f"{z}/{f}=", z/f)
+
+
+# # ismlar = ["shaxzod","davlat","firuz",]
+# # print("salom "+ ismlar[0] + " bugun choyxona bormi?")
+# # print(f"{ismlar[1]} " + f"{ismlar[2]} choyxonaga boramizmi")
+
+# # t_shaxslar =["Amir Temur"]
+
+
+# # a = 5
+# # b = 10
+# # def add():
+# #     print(a+b)
+# # add()    
+
+# # Mavzu: lambda
+
+# # from ast import Lambda
+
+
+# # def hello():
+# #     print("assalom Aleykum uzbekiston")
+# # hello()
+# # lambda:print("assalom Aleykum uzbekiston")
+# # def hello(x):
+#     # print(x)
+# # hello(5)    
+# # (lambda x:print(x**2))(5)
+# # (lambda x:print(x/2))(5)
+
+# # (lambda a:print("a noldan katta") if a > 0 else print("a noldan kichkina"))(5)
+
+# # a = int(input("birinchi sonni kirit:"))
+# # b = int(input("ikkinchi sonni kirit:"))
+# # amal = input("amal kirit:")
+# # if amal =="+":
+# #     (lambda num1,num2:print(num1+num2))(a,b)
+# # elif amal =="-":
+# #     (lambda num1,num2:print(num1-num2))(a,b)
+# # elif amal =="*":
+# #      (lambda num1,num2:print(num1*num2))(a,b)
+# # elif amal =="/":
+# #      (lambda num1,num2:print(num1/num2))(a,b)           
+# # else:
+# #     print("bunaqa amal yuq")
+
+# # 1- misol 
+
+# # python_words = {
+# #     'integer':'Butun son',
+# #     'float': "O'nlik son",
+# #     'boolean':"Mantiqiy qiymat",
+# #     'for':"Biror amalni qayta-qayta bajarish tsikli",
+# #     'if':'Shartlarni tekshirish operatori'}
+
+# # for key, value in sorted(python_words.items()):
+# #     print(f"{key.title()} - {value}")
+# # natija:
+# # Boolean - Mantiqiy qiymat
+# # Float - O'nlik son
+# # For - Biror amalni qayta-qayta bajarish tsikli
+# # If - Shartlarni tekshirish operatori
+# # Integer - Butun son
+
+# # 2- misol
+
+# # davlatlar = {
+# #     "o'zbekiston":'toshkent',
+# #     'aqsh':'washington d.c.',
+# #     'rossiya':'moskva',
+# #     'tojikiston':'dushanbe',
+# #     "qirg'iziston":'bishkek',
+# #     'qozog\'iston':'nursulton',
+# #     'malayziya':'kuala-lumpur',
+# #     'singapur':'sungapur',
+# #     'italiya':'rim'}
+# # for n,i in davlatlar.items():
+# #     print(n,i)
+
+# # Natija:
+# # o'zbekiston toshkent
+# # aqsh washington d.c.
+# # rossiya moskva
+# # tojikiston dushanbe
+# # qirg'iziston bishkek
+# # qozog'iston nursulton
+# # malayziya kuala-lumpur
+# # singapur sungapur
+# # italiya rim
+
+# from turtle import title
+
+
+# # menu = {
+# #     "osh 20000"
+# #     "lag`mon 35000"
+# #     "non 5000"
+# #     "choy 2000"
+# #     "shashlik:12000"
+# #     "somsa 6000"
+# #     "tabaka 23000"
+# # }
+# # print("3 ta taom buyurtma bering")
+# # buyurtmalar = []
+# # for  n in range(3):
+# #     buyurtmalar.append(input(f"{n+1}-taom:").lower())
+
+# # for buyurtma in buyurtmalar:
+# #     if buyurtma in menyu: 
+# #        print(f"{buyurtma.title()} {menu[buyurtma]} so`m")
+# #     else:
+# #         print(f"(kechirasiz, bizda {buyutma} yuq.")
+
+
+# # set
+# # ls =[1,2,3,"hi"]
+# # tp =[1,2,3,45,"hi"]
+# # dicts = {
+# #     "name":"ali"
+# # }
+# # st = {"ali","vali",12,23,}
+# # print(st)
+# # print(dicts)
+
+# # a = {"farrux","zafar","jasur",}
+# # b = {"hilola","farida","dilshoda"}
+# # # a.update(b)
+# # # print(a)
+# # a.remove("farrux")
+# # b.discard("farida")
+# # print(a)
+# # print(b)
+# # a.clear()
+# # b.clear()
+# # print(a)
+# # print(b)
+
+# # a = {"farrux","zafar","jasur"}
+# # a.pop()
+# # print(a)
+
+
+# # import pygame
+
+
+# Mashhur = {
+#     "name":"Shavkat",
+#     "familya":"Mirziyoyev",
+#     "yili":"1952",  
+#     "hozirda": "uzbekiston prizdenti",
+#     "ishlari":"uzbekistonni yashnatyapti",
+    
+# }
+
+# print(Mashhur)
+
+# mashhur1 = {
+#     "name":"Alisher",
+#     "familya":"Usmonov",
+#     "yili":"1958",
+#     "hozirda":"uzbekiston millaneri",
+#     "ishlari":"uzbekiston kambagallarga yordam bergan",
+
+# }
+
+# print(mashhur1)
+
+
+# mashhur2 = {
+#     "name":"Alisher",
+#     "familya":"Uzoqov",
+#     "yili":"1987",
+#     "hozirda":"uzbekiston aktyori",
+#     "ishlari":"uzbekistonda katta boshlovchi ",
+
+# }    
+
+# print(mashhur2)
+
+
+# mashhur3 = {
+#     "name":"Putin",
+#     "familya":"Vladimir",
+#     "yili":"1940",
+#     "hozirda":"Rossiya prizdenti",
+#     "ishlari":"Amerikani bosib olmoqchi ",
+    
+# }   
+
+# mashhur3['asarlar'] = ['Ukrainani yengish usullari','Xitoyga bosimalr']
+# print(mashhur3)
+
+
+
+
+
+# Mavzu: Module
+
+# import king 
+# king.div(4,5)
+
+# from king import add as a,ayir,mult,div
+# a(1,5)
+# ayir(2,3)
+# mult(5,8)
+# div(9,8)
+
+
+# import math
+# print(math.sqrt(15))
+# print(math.pow(23,5))
+
+# import avto_info # avto_info_mod faylini (modulini) chaqiramiz
+
+# avto1 = avto_info.avto_info("GM", "Malibu", "Qora", "avtomat", 2020,40000)
+# avto_info.info_print(avto1)
+
+# savol  = "sevgan kasbingizni kiriting"
+# savol += "(barcha kasblarni kiritib bolgach 'exit'deb yozing):"
+# while True:
+#     kasb = input(savol)
+#     if kasb == "exit" :
+#         print('raxmat!')
+
+
+
+
+
